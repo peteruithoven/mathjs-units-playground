03583f4d9926b3814a14707e70bafdf456e5a107
Peter Uithoven <peter@peteruithoven.nl>
2020-01-03 21:59:41 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      Mathjs units playground
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <section class="d-flex">
+          <section class="flex-grow-1 mr-3">
+            <h2>Source</h2>
+            <v-text-field label="Unit" placeholder="kg" v-model="sourceUnit" />
+            <v-text-field label="Value" v-model="sourceValue" />
+          </section>
+          <section class="flex-grow-1">
+            <h2>Target</h2>
+            <v-text-field label="Unit" placeholder="kg" v-model="targetUnit" />
+            {{ result.value }}
+          </section>
+        </section>
+        <v-alert v-if="result.error" type="error">
+          {{ result.error }}
+        </v-alert>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { create, unitDependencies } from "mathjs";
+
+const math = create({
+  unitDependencies,
+});
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  data: () => ({
+    sourceUnit: "kg",
+    targetUnit: "g",
+    sourceValue: 1,
+  }),
+  computed: {
+    result() {
+      let value = "";
+      let error = "";
+      try {
+        value = math
+          .unit(this.sourceValue, this.sourceUnit)
+          .toNumber(this.targetUnit);
+      } catch (e) {
+        error = e.message;
+      }
+
+      return { value, error };
+    },
+  },
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
